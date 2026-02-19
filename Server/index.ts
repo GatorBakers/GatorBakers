c8d9fcef235b86fe890aa30a2ac47ddfb6d8b2c6
Mar1789 <116681148+Mar1789@users.noreply.github.com>
2026-02-19 15:08:36 -0500

    Fixed importing and added register

--- Server/index.ts
+++ Server/index.ts
@@ -1,10 +1,26 @@
+import "dotenv/config";
 import { PrismaClient } from "./generated/prisma";
 import express, { Request, Response } from "express";
+import { PrismaPg } from '@prisma/adapter-pg'
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+})
+
 import cors from "cors";
 
-const prisma = new PrismaClient();
+export const prisma = new PrismaClient({adapter});
 const app = express();
 const PORT = 4000;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.use(express.json());
 
@@ -18,4 +34,15 @@ app.use(
 
 app.post("/register", async (req: Request, res: Response) => {
   const { email, password, first_name, last_name } = req.body;
+  const user = await prisma.user.create({
+    data: {
+      email: email,
+      password: password,
+      first_name: first_name,
+      last_name: last_name,
+    },
+  });
+  res.json(user);
 });
+
+app.listen(PORT, () => console.log("Server running on port " + PORT));
