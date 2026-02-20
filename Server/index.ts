a54faebaaac34ad5b22347bec330dbea4fda46a2
Mar1789 <116681148+Mar1789@users.noreply.github.com>
2026-02-19 21:35:21 -0500

    Operational Register API

--- Server/index.ts
+++ Server/index.ts
@@ -1,15 +1,16 @@
 import "dotenv/config";
 import { PrismaClient } from "./generated/prisma";
 import express, { Request, Response } from "express";
-import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaPg } from "@prisma/adapter-pg";
+import cors from "cors";
+import bcrypt from 'bcrypt';
 
+const saltRounds = 10;
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
-})
+});
 
-import cors from "cors";
-
-export const prisma = new PrismaClient({adapter});
+export const prisma = new PrismaClient({ adapter });
 const app = express();
 const PORT = 4000;
 
@@ -22,22 +23,13 @@ app.use(
   }),
 );
 
-app.use(express.json());
-
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
-
 app.post("/register", async (req: Request, res: Response) => {
   const { email, password, first_name, last_name } = req.body;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
   const user = await prisma.user.create({
     data: {
       email: email,
-      password: password,
+      password: hashedPassword,
       first_name: first_name,
       last_name: last_name,
     },
