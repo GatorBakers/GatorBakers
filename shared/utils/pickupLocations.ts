baf79734d30dafcf0c8bec31edfded1682a6ae3f
dalva8711 <dalvarezdejesus@gmail.com>
2026-03-12 00:28:43 -0400

    Possible Pickup Locations For Users

---
+++ shared/utils/pickupLocations.ts
@@ -0,0 +1,23 @@
+export interface PickupLocation {
+  name: string;
+  address: string;
+}
+
+export const pickupLocations: PickupLocation[] = [
+  {
+    name: "Marston Science Library",
+    address: "444 Newell Dr, Gainesville, FL 32611",
+  },
+  {
+    name: "Reitz Union",
+    address: "655 Reitz Union Drive, Gainesville, FL 32603",
+  },
+  {
+    name: "Library West",
+    address: "1545 W University Ave, Gainesville, FL 32603",
+  },
+  {
+    name: "Broward Dining Hall",
+    address: "596 Broward Dr, Gainesville, FL 32612",
+  }
+];
