1ebe7d09cef9957da19536158ba9e2da897cc0f3
Mar1789 <116681148+Mar1789@users.noreply.github.com>
2026-03-13 11:23:08 -0400

    Create migration.sql

---
+++ Server/prisma/migrations/20260313150835_init/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - Added the required column `pickup_location` to the `Order` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "Listing_Status" ADD VALUE 'CONFIRMED';
+ALTER TYPE "Listing_Status" ADD VALUE 'COMPLETED';
+ALTER TYPE "Listing_Status" ADD VALUE 'CANCELLED';
+
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "pickup_location" TEXT NOT NULL;
