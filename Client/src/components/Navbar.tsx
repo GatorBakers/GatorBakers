22c61ed2bc366a3ca71ff2b7eb11d0a335b2267b
dalva8711 <dalvarezdejesus@gmail.com>
2026-03-23 20:10:38 -0400

    Changed route name to reflect actual page

--- Client/src/components/Navbar.tsx
+++ Client/src/components/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
                     <img src={SearchIcon} alt="Search" className="navbar-icon" />
                     <span>Search</span>
                 </NavLink>
-                <NavLink to="/orders&listings" className="navbar-link">
+                <NavLink to="/orders" className="navbar-link">
                     <img src={HandbagIcon} alt="Your Orders" className="navbar-icon" />
                     <span>Orders</span>
                 </NavLink>
--- Client/src/pages/CreateListingPage.tsx
+++ Client/src/pages/CreateListingPage.tsx
@@ -98,7 +98,7 @@ const CreateListingPage = () => {
             queryClient.invalidateQueries({ queryKey: ['profile'] });
             setListing(INITIAL_LISTING);
             setResetKey(k => k + 1);
-            navigate('/orders&listings');
+            navigate('/orders');
         } catch (err) {
             setError(err instanceof Error ? err.message : 'Failed to create listing.');
         } finally {
